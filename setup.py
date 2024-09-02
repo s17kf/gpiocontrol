73c430af7b4e976d120027616bcd7b7b5e505374
s17kf <s17kf@users.noreply.github.com>
2024-09-02 20:58:33 +0200

    Description refactor

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='gpiocontrol',
     version='1.0',
-    description="Simple wrapper bash' GPIO pins commands",
+    description="Simple wrapper for bash commands to control GPIO.",
     author='s17kf',
     packages=find_packages(),
 )
