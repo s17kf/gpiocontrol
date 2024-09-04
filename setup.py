05842477926fdbe5747228dad618e267b59e9f3f
s17kf <s17kf@users.noreply.github.com>
2024-09-04 20:10:07 +0200

    Change version to 1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gpiocontrol',
-    version='1.0',
+    version='1.1',
     description="Simple wrapper for bash commands to control GPIO.",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
